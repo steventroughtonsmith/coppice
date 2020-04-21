8ba649677a170a15a35f7aa89a97d4ff90e93779
Martin Pilkington <pilky@mcubedsw.com>
2020-04-21 16:35:41 +0100

    Prep for CanvasListVM tests

---
+++ Bubbles/BubblesTests/Unit Tests/Canvases Editor/CanvasListViewModelTests.swift
@@ -0,0 +1,35 @@
+//
+//  CanvasListViewModelTests.swift
+//  BubblesTests
+//
+//  Created by Martin Pilkington on 21/04/2020.
+//  Copyright © 2020 M Cubed Software. All rights reserved.
+//
+
+import XCTest
+
+class CanvasListViewModelTests: XCTestCase {
+
+    //MARK: - .canvases
+
+
+    //MARK: - .selectedCanvasIndex
+
+
+    //MARK: - selectCanvas(atIndex:)
+
+
+    //MARK: - addPage(with:toCanvasAtIndex:)
+
+
+    //MARK: - addPages(fromFilesAtURLs:toCanvasAtIndex:)
+
+
+    //MARK: - moveCanvas(with:aboveCanvasAtIndex:)
+
+
+    //MARK: - deleteCanvas(atIndex:)
+
+
+    //MARK: - Observation
+}
