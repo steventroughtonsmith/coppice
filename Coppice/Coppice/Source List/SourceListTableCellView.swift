8325f4fbe70a2679d66b5067a6e634e51cb91291
Martin Pilkington <pilky@mcubedsw.com>
2020-07-06 20:12:59 +0100

    COPPICE-229: Dragging selected sidebar item in light mode has wrong text colour

---
+++ Coppice/Coppice/Source List/SourceListTableCellView.swift
@@ -0,0 +1,34 @@
+//
+//  SourceListTableCellView.swift
+//  Coppice
+//
+//  Created by Martin Pilkington on 06/07/2020.
+//  Copyright © 2020 M Cubed Software. All rights reserved.
+//
+
+import AppKit
+
+class SourceListTableCellView: EditableLabelCell {
+    override var draggingImageComponents: [NSDraggingImageComponent] {
+        let components = super.draggingImageComponents
+        guard
+            let textField = self.textField,
+            let newStyle = textField.attributedStringValue.mutableCopy() as? NSMutableAttributedString,
+            let labelIndex = components.firstIndex(where: { $0.key == .label })
+        else {
+            return components
+        }
+
+        //Set to the label colour and draw
+        newStyle.addAttribute(.foregroundColor, value: NSColor.labelColor, range: NSMakeRange(0, newStyle.length))
+        let image = NSImage(size: textField.bounds.size, flipped: false) { (rect) -> Bool in
+            newStyle.draw(in: rect)
+            return true
+        }
+
+        //We'll just update the label contents as everything else is already setup for us
+        components[labelIndex].contents = image
+
+        return components
+    }
+}
