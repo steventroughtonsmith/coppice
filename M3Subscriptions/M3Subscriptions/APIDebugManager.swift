5d338854facfac462b5b8585f1a65268556fe11e
Martin Pilkington <pilky@mcubedsw.com>
2020-09-16 18:53:32 +0100

    COPPICE-279: Create API Debug Mode
    - Added menu items to select debug options

--- M3Subscriptions/M3Subscriptions/APIDebugManager.swift
+++ M3Subscriptions/M3Subscriptions/APIDebugManager.swift
@@ -27,48 +27,115 @@ public class APIDebugManager: NSObject {
         return menu
     }
 
-    //MARK: - Activate
-    var activateDebugString: String? {
-        return nil
+    private func updateState(of menu: NSMenu?, value: String?) {
+        guard let menu = menu else {
+            return
+        }
+
+        for item in menu.items {
+            let representedString = item.representedObject as? String
+            item.state = (representedString == value) ? .on : .off
+        }
     }
+
+    //MARK: - Activate
+    var activateDebugString: String?
 
     private func buildActivateMenu() -> NSMenu {
         let menu = NSMenu()
+        let none = menu.addItem(withTitle: "None", action: #selector(activateMenuItemSelected(_:)), keyEquivalent: "")
+        none.target = self
+        menu.addItem(NSMenuItem.separator())
+
+        let noSubscription = menu.addItem(withTitle: "No Subscription Found", action: #selector(activateMenuItemSelected(_:)), keyEquivalent: "")
+        noSubscription.representedObject = "no_subscription_found"
+        noSubscription.target = self
+
+        let multipleSubscriptions2 = menu.addItem(withTitle: "2 Subscriptions Found", action: #selector(activateMenuItemSelected(_:)), keyEquivalent: "")
+        multipleSubscriptions2.representedObject = "multiple_subscriptions::number:2"
+        multipleSubscriptions2.target = self
+        let multipleSubscriptions5 = menu.addItem(withTitle: "5 Subscription Found", action: #selector(activateMenuItemSelected(_:)), keyEquivalent: "")
+        multipleSubscriptions5.representedObject = "multiple_subscriptions::number:5"
+        multipleSubscriptions5.target = self
+
+        let subscriptionExpiredCancelled = menu.addItem(withTitle: "Subscription Expired (Cancelled)", action: #selector(activateMenuItemSelected(_:)), keyEquivalent: "")
+        subscriptionExpiredCancelled.representedObject = "subscription_expired::renewalStatus:cancelled"
+        subscriptionExpiredCancelled.target = self
+        let subscriptionExpiredFailed = menu.addItem(withTitle: "Subscription Expired (Billing Failed)", action: #selector(activateMenuItemSelected(_:)), keyEquivalent: "")
+        subscriptionExpiredFailed.representedObject = "subscription_expired::renewalStatus:failed"
+        subscriptionExpiredFailed.target = self
+
+        let tooManyDevices = menu.addItem(withTitle: "Too Many Devices", action: #selector(activateMenuItemSelected(_:)), keyEquivalent: "")
+        tooManyDevices.representedObject = "too_many_devices::number:3"
+        tooManyDevices.target = self
+
+        self.updateState(of: menu, value: self.activateDebugString)
+
         return menu
     }
 
     @IBAction func activateMenuItemSelected(_ sender: NSMenuItem?) {
-
+        self.activateDebugString = sender?.representedObject as? String
+        self.updateState(of: sender?.menu, value: self.activateDebugString)
     }
 
 
     //MARK: - Deactivate
-    var deactivateDebugString: String? {
-        return nil
-    }
+    var deactivateDebugString: String?
 
     private func buildDeactivateMenu() -> NSMenu {
         let menu = NSMenu()
+        let none = menu.addItem(withTitle: "None", action: #selector(deactivateMenuItemSelected(_:)), keyEquivalent: "")
+        none.target = self
+        menu.addItem(NSMenuItem.separator())
+
+        let noDevice = menu.addItem(withTitle: "No Device Found", action: #selector(deactivateMenuItemSelected(_:)), keyEquivalent: "")
+        noDevice.representedObject = "no_device_found"
+        noDevice.target = self
+
+        self.updateState(of: menu, value: self.activateDebugString)
+
         return menu
     }
 
     @IBAction func deactivateMenuItemSelected(_ sender: NSMenuItem?) {
-
+        self.deactivateDebugString = sender?.representedObject as? String
+        self.updateState(of: sender?.menu, value: self.deactivateDebugString)
     }
 
 
     //MARK: - Check
-    var checkDebugString: String? {
-        return nil
-    }
+    var checkDebugString: String?
 
     private func buildCheckMenu() -> NSMenu {
         let menu = NSMenu()
+        let none = menu.addItem(withTitle: "None", action: #selector(checkMenuItemSelected(_:)), keyEquivalent: "")
+        none.target = self
+        menu.addItem(NSMenuItem.separator())
+
+        let noDevice = menu.addItem(withTitle: "No Device Found", action: #selector(checkMenuItemSelected(_:)), keyEquivalent: "")
+        noDevice.representedObject = "no_device_found"
+        noDevice.target = self
+
+        let noSubscription = menu.addItem(withTitle: "No Subscription Found", action: #selector(checkMenuItemSelected(_:)), keyEquivalent: "")
+        noSubscription.representedObject = "no_subscription_found"
+        noSubscription.target = self
+
+        let subscriptionExpiredCancelled = menu.addItem(withTitle: "Subscription Expired (Cancelled)", action: #selector(checkMenuItemSelected(_:)), keyEquivalent: "")
+        subscriptionExpiredCancelled.representedObject = "subscription_expired::renewalStatus:cancelled"
+        subscriptionExpiredCancelled.target = self
+        let subscriptionExpiredFailed = menu.addItem(withTitle: "Subscription Expired (Billing Failed)", action: #selector(checkMenuItemSelected(_:)), keyEquivalent: "")
+        subscriptionExpiredFailed.representedObject = "subscription_expired::renewalStatus:failed"
+        subscriptionExpiredFailed.target = self
+
+        self.updateState(of: menu, value: self.activateDebugString)
+
         return menu
     }
 
     @IBAction func checkMenuItemSelected(_ sender: NSMenuItem?) {
-
+        self.checkDebugString = sender?.representedObject as? String
+        self.updateState(of: sender?.menu, value: self.checkDebugString)
     }
 }
 #endif
