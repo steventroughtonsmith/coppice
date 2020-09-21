6ac700c63bc1bc6645dd11308b8ed0a44699f6dd
Martin Pilkington <pilky@mcubedsw.com>
2020-09-21 14:02:48 +0100

    Post-2020.1 beta 2

--- Scripts/package_and_notarize.sh
+++ Scripts/package_and_notarize.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 
-ARCHIVE_PATH="~/Library/Developer/Xcode/Archives/$1"
+ARCHIVE_PATH="$1"
+
+echo "===Exporting Build from $ARCHIVE_PATH==="
 
 rm -rf "build"
 mkdir "build"
 xcodebuild -exportArchive -archivePath "$ARCHIVE_PATH" -exportOptionsPlist "ExportOptions.plist" -exportPath "build"
 
-mkdir "export"
+echo "===Creating DMG ==="
 DMG_PATH=$(dropdmg --config-name=Coppice --destination=export build/Coppice.app)
-rm -rf "build"
 
-xcrun altool --notarize-app --primary-build-id "com.mcubedsw.coppice" --username "pilky@mcubedsw.com" --password "@keychain:Notarization" --file "$DMG_PATH"
+echo "===Sending to Notarization==="
+xcrun altool --notarize-app -f "$DMG_PATH" --primary-bundle-id "com.mcubedsw.coppice" -u "pilky@mcubedsw.com" -p "@keychain:Notarization" --asc-provider "MartinPilkington74170789" 
 
-open "$DMG_PATH"
+# open "$DMG_PATH"
--- Scripts/sign_sparkle.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-SPARKLE_PATH="$1"
-
-$SPARKLE_PATH/bin/codesign_embedded_executable "Developer ID Application: M Cubed Software Ltd. (K3K39KWKBV)" XPCServices/*.xpc
-$SPARKLE_PATH/bin/codesign_embedded_executable "Developer ID Application: M Cubed Software Ltd. (K3K39KWKBV)" ./Sparkle.framework/Versions/A/Resources/Autoupdate
-$SPARKLE_PATH/bin/codesign_embedded_executable "Developer ID Application: M Cubed Software Ltd. (K3K39KWKBV)" ./Sparkle.framework/Versions/A/Resources/Updater.app/
-
-echo "Signed!"
