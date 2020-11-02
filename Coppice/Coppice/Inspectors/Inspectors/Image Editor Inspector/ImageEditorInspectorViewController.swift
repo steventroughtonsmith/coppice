b87e09b1b3f7978f8a83f53277c2268ed5697411
Martin Pilkington <pilky@mcubedsw.com>
2020-11-02 11:12:59 +0000

    COPPICE-310: Image Inspector’s Description should grow as the user types

--- Coppice/Coppice/Inspectors/Inspectors/Image Editor Inspector/ImageEditorInspectorViewController.swift
+++ Coppice/Coppice/Inspectors/Inspectors/Image Editor Inspector/ImageEditorInspectorViewController.swift
@@ -14,4 +14,31 @@ class ImageEditorInspectorViewController: BaseInspectorViewController {
     }
 
     override var ranking: InspectorRanking { return .content }
+    @IBOutlet var descriptionScrollViewHeight: NSLayoutConstraint!
+    @IBOutlet var descriptionTextView: NSTextView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.updateTextViewHeight()
+    }
+
+    private func updateTextViewHeight() {
+        guard
+            let layoutManager = self.descriptionTextView.layoutManager,
+            let textContainer = self.descriptionTextView.textContainer
+        else {
+            return
+        }
+
+        //We need to add 1pt at the top and bottom to account for the border
+        let height = layoutManager.usedRect(for: textContainer).height + 2
+
+        self.descriptionScrollViewHeight.constant = max(60, min(height, 150))
+    }
+}
+
+extension ImageEditorInspectorViewController: NSTextViewDelegate {
+    func textDidChange(_ notification: Notification) {
+        self.updateTextViewHeight()
+    }
 }
